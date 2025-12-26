cad2a5f2a3a634af3f62d4dce56f0ef61a42005f
KhaledEzzet <khalid.azet@gmail.com>
2025-12-27 00:07:59 +0200

    feat: update app icons and implement cached network images for currency flags

--- lib/feature/convert/view/widgets/currency_flag.dart
+++ lib/feature/convert/view/widgets/currency_flag.dart
@@ -1,29 +1,52 @@
+import 'package:cached_network_image/cached_network_image.dart';
 import 'package:flutter/material.dart';
 
 const double _flagWidth = 24;
 const double _flagHeight = 16;
 
 class CurrencyFlag extends StatelessWidget {
-  const CurrencyFlag({super.key, required this.code});
+  const CurrencyFlag({required this.code, super.key});
 
   final String? code;
 
   @override
   Widget build(BuildContext context) {
     if (code == null || code!.isEmpty) {
-      return const SizedBox(width: _flagWidth, height: _flagHeight);
+      return const _FlagPlaceholder();
     }
+
+    final devicePixelRatio = MediaQuery.of(context).devicePixelRatio;
+    final cacheWidth = (_flagWidth * devicePixelRatio).round();
+    final cacheHeight = (_flagHeight * devicePixelRatio).round();
 
     return ClipRRect(
       borderRadius: BorderRadius.circular(2),
-      child: Image.network(
-        'https://flagcdn.com/w20/$code.png',
+      child: CachedNetworkImage(
+        imageUrl: 'https://flagcdn.com/w20/$code.png',
         width: _flagWidth,
         height: _flagHeight,
         fit: BoxFit.cover,
-        errorBuilder: (_, __, ___) {
-          return const SizedBox(width: _flagWidth, height: _flagHeight);
-        },
+        memCacheWidth: cacheWidth,
+        memCacheHeight: cacheHeight,
+        filterQuality: FilterQuality.high,
+        placeholder: (_, __) => const _FlagPlaceholder(),
+        errorWidget: (_, __, ___) => const _FlagPlaceholder(),
+      ),
+    );
+  }
+}
+
+class _FlagPlaceholder extends StatelessWidget {
+  const _FlagPlaceholder();
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: _flagWidth,
+      height: _flagHeight,
+      decoration: BoxDecoration(
+        color: Theme.of(context).colorScheme.surfaceContainerHighest,
+        borderRadius: BorderRadius.circular(2),
       ),
     );
   }
